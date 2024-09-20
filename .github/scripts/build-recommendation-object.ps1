64855823c17e03f01e106b74dbaa3cafc194aadb
Kyle Poineal <38540295+kpoineal@users.noreply.github.com>
2024-09-20 13:04:46 -0600

    test3

--- .github/scripts/build-recommendation-object.ps1
+++ .github/scripts/build-recommendation-object.ps1
@@ -30,7 +30,7 @@ function Build-APRLJsonObject {
 
 #Try to build and export the object. If it fails, catch the error and exit with code 1
 try{
-  Build-APRLJsonObject -path "./azure-resources" | ConvertTo-Json -Depth 10 | Out-File -FilePath "../tools/recommendations.json" -Force
+  Build-APRLJsonObject -path "./azure-resources" | ConvertTo-Json -Depth 10 | Out-File -FilePath "./tools/data/recommendations.json" -Force
   exit 0
 }
 catch{
